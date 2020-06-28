631cc765742d1976deef66ddaf96017f9f197fa4
DenysFerreira <lerroandeco@gmail.com>
2020-06-28 20:11:40 -0300

    Behind the scenes - Demo

--- my-first-angular-project/src/app/app.component.ts
+++ my-first-angular-project/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is my first App';
+
+  firstName = "Denys";
+  lastName = "Lerroan";
 }
